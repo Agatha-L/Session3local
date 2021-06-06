252546167d0524c95ca7efb760d073233ac4b9b4
Agatha Laurent <jndaliagatha@gmail.com>
2021-06-06 12:47:04 -0700

    updated sess3ans

--- Program.cs
+++ Program.cs
@@ -9,19 +9,60 @@ namespace Session3App
             //Console.WriteLine("Hello World!");
 
             String [] Menu = new String [4] {"Robster Miano", "Smashed Potato Salad", "Vegan Sushi", "Grilled Salmon"};
+            Menu[0] = "Robster Miano";
+            Menu[1] = "Smashed Potato Salad";
+            Menu[2] = "Vegan Sushi";
+            Menu[3] = "Grilled Salmon";
 
-            Double [] Prices = new Double [4] {256.4, 102.1, 152.8, 93.9};
+            float [] Prices = new float [4] {256.4f, 102.1f, 152.8f, 93.9f};
+            Prices [0] = 256.4f;
+            Prices [1] = 102.1f;
+            Prices [2] = 152.8f;
+            Prices [3] = 93.9f;
 
-            foreach (String food in Menu)
+
+            int j;
+                             
+            for(j = 0; j < Menu.Length; j++)
+                        
             {
-                Double j;
-                for (j = 0; j < Prices.Length; j++ )
+                foreach(var k in Prices)
                 {
-                    Console.WriteLine(" Food: [{0}] - Price:{0}$", Menu, Prices);
+
+                    // Console.WriteLine(" Food:{0} - Price:{1}$", Menu[j], Prices[k]);  why thise2 failed
+                    // break;
+                    // Console.WriteLine($"Food: {Menu} - Price:{Prices}$");
+                    // break;
+                    Console.WriteLine("Food: " + Menu[j]+ " - Price: "+ Prices[j] +"$");
+                    break;
+                    
+                }   
+
+            }
+
+
+            Console.WriteLine("\nJohn has 150.00$ ");
+
+            for(j = 0; j < Menu.Length; j++)
+            {
+                int n = 0;
+                if (Prices [n] <= 150.0f )
+                {
+                    Console.WriteLine("He can only afford eating: " + Menu[j]);
+                    //n++;
+                    break;
 
                 }
-                           
-            }
+
+                else
+                {
+                    Console.WriteLine("He cannot afford eating: " + Menu[j]);
+                    break;
+                }
+
+            } 
+
+
 
             
 
